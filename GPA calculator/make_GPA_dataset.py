e2d0eadf753b0ab4eb84b8735d3b75fa980ecf93
Lucas Pierru <lucas.pierru@epitech.eu>
2019-03-24 19:04:52 +0100

    Quick algorithm fix. Updated GPA.xlsx

--- GPA calculator/make_GPA_dataset.py
+++ GPA calculator/make_GPA_dataset.py
@@ -7,7 +7,7 @@ xlsx = pd.read_excel(filePath, usecols=[0, 4, 5])
 xlsx = xlsx.sort_values(['Identifiant élève'], ascending=False)
 xlsx = xlsx[xlsx['Grade obtenu'] != '-']
 xlsx = xlsx[xlsx['Grade obtenu'] != 'Acquis']
-xlsx['Grade obtenu'] = xlsx['Grade obtenu'].map({'A': 4, 'B': 3, 'C': 2, 'D': 4, 'Echec': 0})
+xlsx['Grade obtenu'] = xlsx['Grade obtenu'].map({'A': 4, 'B': 3, 'C': 2, 'D': 1, 'Echec': 0})
 
 resultDF = pd.DataFrame()
 studentID = xlsx.iloc[0, 0]
@@ -16,12 +16,12 @@ totalCreditsNb = 0
 tmp = 0
 
 # Reminder
-# id = row[0]
-# grade = row[1]
-# creditsNb = row[2]
+# id --> row[0]
+# grade --> row[1]
+# creditsNb --> row[2]
 
 for index, row in xlsx.iterrows():
-    if (studentID == row[0]):
+    if studentID == row[0]:
         tmp += row[1] * row[2]
         totalCreditsNb += row[2]
     else:
@@ -29,6 +29,8 @@ for index, row in xlsx.iterrows():
         resultDF = resultDF.append([(studentID, GPA)], ignore_index=True)
         tmp = 0
         totalCreditsNb = 0
+        tmp += row[1] * row[2]
+        totalCreditsNb += row[2]
         studentID = row[0]
 
 resultDF.to_excel("GPA.xlsx")
