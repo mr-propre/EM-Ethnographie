768ef1a6fb6d86af9ec3c3db3ea7458262dd7cb9
Lucas Pierru <lucas.pierru@epitech.eu>
2019-03-24 18:07:17 +0100

    Working and complete GPA calculation. Add the GPA.xlsx generated dataset and pip's requirements.txt.

--- GPA calculator/make_GPA_dataset.py
+++ GPA calculator/make_GPA_dataset.py
@@ -3,18 +3,32 @@ import pandas as pd
 import sys
 
 filePath = sys.argv[1]
-xlsx = pd.read_excel(filePath, index_col=0, usecols=[0, 4, 5])
+xlsx = pd.read_excel(filePath, usecols=[0, 4, 5])
 xlsx = xlsx.sort_values(['Identifiant élève'], ascending=False)
 xlsx = xlsx[xlsx['Grade obtenu'] != '-']
-xlsx = xlsx['Grade obtenu'].replace(['A'], '4')
-xlsx = xlsx['Grade obtenu'].replace(['B'], '3')
-xlsx = xlsx['Grade obtenu'].replace(['C'], '2')
-xlsx = xlsx['Grade obtenu'].replace(['D'], '1')
+xlsx = xlsx[xlsx['Grade obtenu'] != 'Acquis']
+xlsx['Grade obtenu'] = xlsx['Grade obtenu'].map({'A': 4, 'B': 3, 'C': 2, 'D': 4, 'Echec': 0})
 
-print(xlsx)
+resultDF = pd.DataFrame()
+studentID = xlsx.iloc[0, 0]
+GPA = 0
+totalCreditsNb = 0
+tmp = 0
 
-xlsxLen = xlsx['Identifiant élève'].count()
-i = 0
-while (i < xlsxLen):
-    # Build of the new GPA dataFrame
-    i += 1
+# Reminder
+# id = row[0]
+# grade = row[1]
+# creditsNb = row[2]
+
+for index, row in xlsx.iterrows():
+    if (studentID == row[0]):
+        tmp += row[1] * row[2]
+        totalCreditsNb += row[2]
+    else:
+        GPA = tmp / totalCreditsNb
+        resultDF = resultDF.append([(studentID, GPA)], ignore_index=True)
+        tmp = 0
+        totalCreditsNb = 0
+        studentID = row[0]
+
+resultDF.to_excel("GPA.xlsx")
