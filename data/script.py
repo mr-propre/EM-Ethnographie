ea613d8f3269290159cb87dc8a5c05a7bcaec8a0
magellan <magellan.chaux-limantour@epitech.eu>
2019-03-26 09:16:00 +0100

    dataset gpa

--- data/script.py
+++ data/script.py
@@ -5,19 +5,20 @@ import pandas
 import itertools
 from datetime import datetime
 
-df = pandas.read_csv("data/gephi_binomes_time.csv", delimiter=';', header=1)
-ref = pandas.read_csv("data/cluster_1.csv", delimiter=';', header=0, names=["id","cluster","city"])
+df = pandas.read_csv("GPA.csv", delimiter=';', header=1)
+ref = pandas.read_csv("cluster_1.csv", delimiter=';', header=0, names=["id","cluster","city"])
 
 tab = []
 
-f = open("data/gephi_binomes_clustered.csv", "w")
+f = open("gephi_gpa.csv", "w")
 
 for index,row in df.iterrows():
     for inde,ro in ref.iterrows():
-        if (ro["id"] == row[0]):
-            for value in row:
-                f.write(str(value) + ";")
-            f.write(str(ro["cluster"]) + "\n")
+        if (ro["id"] == row[1]):
+            f.write(row[1] + ";" + row[2].replace(",", ".") + "\n")
+            #for value in row:
+            #    f.write(str(value) + ";")
+            #f.write(str(ro["cluster"]) + "\n")
 
 f.close()
 
